faf371b2e87f6e43488d736f7099f04d570c8b03
Igor Camilo <igor.rcamilo@gmail.com>
2017-08-18 14:11:08 -0300

    Weather iOS project created.

---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Igor Camilo on 18/08/17.
+//  Copyright © 2017 Igor Camilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weather/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Weather
+//
+//  Created by Igor Camilo on 18/08/17.
+//  Copyright © 2017 Igor Camilo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
